ac388d67c7225247acd7968f36713b03bf381cd0
ychebotaev <yury.79120345101@gmail.com>
2024-03-19 22:46:21 +0500

    Add support for pg in knexfile

--- persistence/knexfile.ts
+++ persistence/knexfile.ts
@@ -1,10 +1,31 @@
-import path from 'node:path'
+import 'dotenv/config'
+import path from "node:path";
 import { type Knex } from "knex";
 
-export default {
-  client: "sqlite3",
-  connection: {
-    filename: path.join(__dirname, "dev.sqlite3"),
-  },
-  useNullAsDefault: true,
-} satisfies Knex.Config;
+const user = process.env["PG_USER"];
+const password = process.env["PG_PASSWORD"];
+const database = process.env["PG_DB"]
+
+let config: Knex.Config;
+
+if (user && password && database) {
+  config = {
+    client: "pg",
+    connection: {
+      host: "localhost",
+      user,
+      password,
+      database,
+    },
+  } satisfies Knex.Config;
+} else {
+  config = {
+    client: "sqlite3",
+    connection: {
+      filename: path.resolve(__dirname, "dev.sqlite3"),
+    },
+    useNullAsDefault: true
+  } satisfies Knex.Config;
+}
+
+export default config;
